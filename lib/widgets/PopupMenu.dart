52562ee31af726b6b92d4bbc95dd33872ba536f1
eringnwn <105049956+eringnwn@users.noreply.github.com>
2023-06-13 16:17:42 +0700

    Update PopupMenu.dart
    fix textcontroller utk edit produk

--- lib/widgets/PopupMenu.dart
+++ lib/widgets/PopupMenu.dart
@@ -5,34 +5,41 @@ import 'package:tim_apel/providers/form_handler/produk_form_provider.dart';
 import '../providers/produk_provider.dart';
 import '../screens/EditProduk.dart';
 
-class PopupMenu extends StatelessWidget {
+class PopupMenu extends StatefulWidget {
   final int idxProduk;
   const PopupMenu({super.key, required this.idxProduk});
 
   @override
+  State<PopupMenu> createState() => _PopupMenuState();
+}
+
+class _PopupMenuState extends State<PopupMenu> {
+  @override
   Widget build(BuildContext context) {
     var produkProv = Provider.of<ProdukProvider>(context);
     var formProv = Provider.of<ProdukFormProvider>(context);
-    var toBeEdited = produkProv.getProduk(idxProduk - 1);
-    
-    void updateFormProvider(){
+    var toBeEdited = produkProv.getProduk(widget.idxProduk - 1);
+    var nama = toBeEdited.stok;
+
+    void _updateFormProvider(){
       formProv.namaProdukController.text = toBeEdited.nama;
       formProv.deskripsiController.text = toBeEdited.deskripsi;
-      formProv.stokController.text = toBeEdited.stok;
-      formProv.hargaJualController.text = toBeEdited.hargaJual;
-      formProv.hargaBeliController.text = toBeEdited.hargaBeli;
+      formProv.stokController.text = toBeEdited.stok.toString();
+      formProv.hargaJualController.text = toBeEdited.hargaJual.toString();
+      formProv.hargaBeliController.text = toBeEdited.hargaBeli.toString();
       formProv.updateKategori = toBeEdited.kategori;
-      formProv.updateEditIdx = toBeEdited.id - 1;
+      formProv.updateEditIdx = 1;
     }
 
     return PopupMenuButton(
       icon: const Icon(Icons.more_vert),
       onSelected: (value) {
         if (value == 'edit'){
-          updateFormProvider();
+          _updateFormProvider();
+          
           Navigator.push(
             context, 
-            MaterialPageRoute(builder: (context) => EditProduk(idxProduk: idxProduk,), 
+            MaterialPageRoute(builder: (context) => EditProduk(idxProduk: widget.idxProduk,), 
             fullscreenDialog: true),
           );
         }
@@ -48,7 +55,6 @@ class PopupMenu extends StatelessWidget {
                 leading: const Icon(Icons.edit),
                 title: const Text('Edit'),
           )),
-          const PopupMenuDivider(),
           const PopupMenuItem(
             value: 'hapus',
             child: ListTile(
