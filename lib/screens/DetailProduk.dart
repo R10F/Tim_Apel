7bfb342983439e57951d435e365751c66d44c0e8
fdavidsen <f.davidsen19@gmail.com>
2023-06-15 23:55:05 +0700

    Refactoring

--- lib/screens/DetailProduk.dart
+++ lib/screens/DetailProduk.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:input_quantity/input_quantity.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:tim_apel/providers/qtyProduk_provider.dart';
 import 'package:tim_apel/models/produk_data_model.dart';
 import 'package:tim_apel/providers/transaksi_provider.dart';
-
-import '../utilities/formatting.dart';
+import 'package:tim_apel/utilities/formatting.dart';
 
 class DetailProduk extends StatefulWidget {
   final Produk produk;
