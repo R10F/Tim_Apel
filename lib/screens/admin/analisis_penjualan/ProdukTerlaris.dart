86e7ca2907dc2f05d206ceb4027c7c248d3c033c
eringnwn <eringnwn@gmail.com>
2023-06-25 22:50:24 +0700

    attempt 1: logic produk terlaris

--- lib/screens/admin/analisis_penjualan/ProdukTerlaris.dart
+++ lib/screens/admin/analisis_penjualan/ProdukTerlaris.dart
@@ -1,26 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tim_apel/providers/account_provider.dart';
+import 'package:tim_apel/providers/produk_provider.dart';
+import 'package:tim_apel/providers/transaksi_provider.dart';
 
-class ProdukTerlaris extends StatefulWidget {
+class ProdukTerlaris extends StatelessWidget {
   const ProdukTerlaris({super.key});
 
   @override
-  State<ProdukTerlaris> createState() => _ProdukTerlarisState();
-}
-
-class _ProdukTerlarisState extends State<ProdukTerlaris> {
-  @override
   Widget build(BuildContext context) {
     var accountProvider = Provider.of<AccountProvider>(context);
+    var transaksiProv = Provider.of<TransaksiProvider>(context);
+    var produkProv = Provider.of<ProdukProvider>(context);
+
+    List<int> getProdukLaris(String kategori){
+      List<int> ids = produkProv.semuaProduk.where((produk) => produk.kategori == kategori).toList().map((e) => e.id,).toList();
+
+      List<Map<int,int>> transaksi = transaksiProv.listTransaksi.map((e) => e.listProduk,).toList();
+      Map<int,int> terjual = {};
+
+      for (Map<int,int> listProduk in transaksi) {
+        listProduk.forEach((id, qty) {
+          if (ids.contains(id)) {
+            terjual.update(id, (value) => terjual[id]! - qty, ifAbsent: () => -qty,); //- karena sortingnya kecil ke besar
+          }
+        });
+      }
+
+      var temp = terjual.entries.toList()..sort((a,b) => a.value.compareTo(b.value));
+      terjual = Map<int,int>.fromEntries(temp);
+      List<int> sliced = terjual.keys.toList();
+
+      sliced.take(3);
+      return sliced;
+    }
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text("Produk Terlaris"),
       ),
       body: SingleChildScrollView(
-        child: Column(
+        child:
+        Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+          children:
+          [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
               child: TextField(
@@ -30,65 +54,93 @@ class _ProdukTerlarisState extends State<ProdukTerlaris> {
                     suffixIcon: const Icon(Icons.search)),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Text(
-                "ATK",
-                style: TextStyle(
-                  fontSize: 23,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 75,
-                      height: 75,
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                            color: Colors.green[500], borderRadius: BorderRadius.circular(5)),
-                        child: Center(
-                          child: Text(
-                            "1",
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: accountProvider.getSetting('dark_mode')
-                                  ? Colors.black
-                                  : Colors.white,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
+            for (int i=0; i<produkProv.kategori.length; i++)
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    "${produkProv.kategori[i]}",
+                    style: TextStyle(
+                      fontSize: 23,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            "Lem Lilin Uk. Kecil",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Text(
-                            "terjual: 100/bulan",
-                            style: TextStyle(fontSize: 15, color: Colors.green[500]),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+                  ),
                 ),
-              ),
-            )
-          ],
+                //call new widget utk render semua isi
+                //LarisWidget(ids: getProdukLaris(kategori[i]))
+                //ids = id dari produk yang terlaris tsb ygy
+              ],
+            ),
+          ]
+          // [
+          //   Padding(
+          //     padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+          //     child: TextField(
+          //       decoration: InputDecoration(
+          //           border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+          //           labelText: 'Nama Produk',
+          //           suffixIcon: const Icon(Icons.search)),
+          //     ),
+          //   ),
+          //   const Padding(
+          //     padding: EdgeInsets.symmetric(horizontal: 25),
+          //     child: Text(
+          //       "ATK",
+          //       style: TextStyle(
+          //         fontSize: 23,
+          //       ),
+          //     ),
+          //   ),
+          //   Padding(
+          //     padding: const EdgeInsets.all(8.0),
+          //     child: Card(
+          //       child: Row(
+          //         children: [
+          //           SizedBox(
+          //             width: 75,
+          //             height: 75,
+          //             child: DecoratedBox(
+          //               decoration: BoxDecoration(
+          //                   color: Colors.green[500], borderRadius: BorderRadius.circular(5)),
+          //               child: Center(
+          //                 child: Text(
+          //                   "1",
+          //                   style: TextStyle(
+          //                     fontSize: 30,
+          //                     fontWeight: FontWeight.bold,
+          //                     color: accountProvider.getSetting('dark_mode')
+          //                         ? Colors.black
+          //                         : Colors.white,
+          //                   ),
+          //                   textAlign: TextAlign.center,
+          //                 ),
+          //               ),
+          //             ),
+          //           ),
+          //           Column(
+          //             crossAxisAlignment: CrossAxisAlignment.start,
+          //             children: [
+          //               const Padding(
+          //                 padding: EdgeInsets.all(8.0),
+          //                 child: Text(
+          //                   "Lem Lilin Uk. Kecil",
+          //                   style: TextStyle(fontSize: 16),
+          //                 ),
+          //               ),
+          //               Padding(
+          //                 padding: const EdgeInsets.symmetric(horizontal: 8),
+          //                 child: Text(
+          //                   "terjual: 100/bulan",
+          //                   style: TextStyle(fontSize: 15, color: Colors.green[500]),
+          //                 ),
+          //               ),
+          //             ],
+          //           )
+          //         ],
+          //       ),
+          //     ),
+          //   )
+          // ],
         ),
       ),
     );
